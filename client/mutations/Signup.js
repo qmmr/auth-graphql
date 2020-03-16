0366fd266746bad093ed6e658084982c0cfc2d77
Marcin Kumorek <kumeek@gmail.com>
2020-03-16 23:09:18 +0100

    Fix signup mutation :see_no_evil:

--- client/mutations/Signup.js
+++ client/mutations/Signup.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag'
 
 const SIGNUP = gql`
   mutation Signup($email: String, $password: String) {
-    login(email: $email, password: $password) {
+    signup(email: $email, password: $password) {
       id
       email
     }
